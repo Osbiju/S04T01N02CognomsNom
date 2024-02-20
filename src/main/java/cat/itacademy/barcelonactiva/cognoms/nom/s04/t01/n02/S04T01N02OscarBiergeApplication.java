96f3202f07b11e011e4edd45aef8cc8cd1ba6ebe
Oscar <okeshik@protonmail.com>
2024-02-20 10:35:17 +0100

    refactor name project

--- src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/S04T01N02OscarBiergeApplication.java
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/S04T01N02OscarBiergeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class S04T01N02CognomsNomApplication {
+public class S04T01N02OscarBiergeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(S04T01N02CognomsNomApplication.class, args);
+		SpringApplication.run(S04T01N02OscarBiergeApplication.class, args);
 	}
 
 }
